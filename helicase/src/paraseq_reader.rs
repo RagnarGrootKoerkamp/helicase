16d92f016fa07fed7aee083a8e4dce351da8e50b
rickbeeloo <r.beeloo@outlook.com>
2026-01-09 12:08:46 +0100

    use `\n@` -> \n not(@)`

--- helicase/src/paraseq_reader.rs
+++ helicase/src/paraseq_reader.rs
@@ -20,6 +20,45 @@ impl<'a, const CONFIG: Config> ParallelHelicaseReader<'a, CONFIG> {
     }
 }
 
+#[inline(always)]
+fn find_fasta_boundary(data: &[u8], mut pos: usize) -> usize {
+    while let Some(offset) = memchr::memchr(b'>', &data[pos..]) {
+        let abs_pos = pos + offset;
+        // Must be preceded by \n
+        if abs_pos > 0 && data[abs_pos - 1] == b'\n' {
+            return abs_pos;
+        }
+        pos = abs_pos + 1;
+    }
+    data.len()
+}
+
+#[inline(always)]
+fn find_fastq_boundary(data: &[u8], mut pos: usize) -> usize {
+    while let Some(offset) = memchr::memchr(b'\n', &data[pos..]) {
+        let abs_pos = pos + offset;
+        // \n must be followed by '@'
+        if abs_pos + 1 >= data.len() || data[abs_pos + 1] != b'@' {
+            pos = abs_pos + 1;
+            continue;
+        }
+        // Check next line after '@'
+        let remaining = &data[abs_pos..];
+        // Search for the newline after the '@' (skipping \n and @)
+        if let Some(newline_offset) = memchr::memchr(b'\n', &remaining[2..]) {
+            let next_line_start = newline_offset + 3; // +2 offset + 1 for the \n itself
+            // Continue search if we have a next line that starts with '@'
+            // i.e. we have quality scores instead of valid record
+            if next_line_start < remaining.len() && remaining[next_line_start] == b'@' {
+                pos = abs_pos + 1;
+                continue;
+            }
+        }
+        return abs_pos + 1;
+    }
+    data.len()
+}
+
 impl<'b, const CONFIG: Config> paraseq::parallel::ParallelReader
     for ParallelHelicaseReader<'b, CONFIG>
 {
@@ -31,55 +70,46 @@ impl<'b, const CONFIG: Config> paraseq::parallel::ParallelReader
     {
         let data = self.mmap.data();
         assert!(data[0] == b'>' || data[0] == b'@');
-        let start = data[0];
+        let is_fastq = data[0] == b'@';
 
         std::thread::scope(|scope| {
-            let len = data.len().div_ceil(num_threads);
+            let chunk_size = data.len().div_ceil(num_threads);
 
             let mut splits = (0..=num_threads)
-                .map(|i| (i * len).min(data.len()))
+                .map(|i| (i * chunk_size).min(data.len()))
                 .collect::<Vec<usize>>();
-            for x in &mut splits {
-                if *x == 0 {
-                    continue;
-                }
-                if *x >= data.len() {
-                    continue;
-                }
-                // find first > or @ preceded by \n after x
-                // TODO: Do this inside worker threads instead?
-                // Or start threads as soon as each split is found?
-                loop {
-                    if let Some(pos) = memchr::memchr(start, &data[*x..]) {
-                        if data[*x + pos - 1] == b'\n' {
-                            *x += pos;
-                            break;
-                        }
-                        *x += pos + 1;
-                    } else {
-                        *x = data.len();
-                        break;
-                    }
-                }
+
+            let len = splits.len();
+            for split in &mut splits[1..len - 1] {
+                *split = if is_fastq {
+                    find_fastq_boundary(data, *split)
+                } else {
+                    find_fasta_boundary(data, *split)
+                };
             }
 
-            let mut handles = vec![];
-            for i in 0..num_threads {
+            // Could end up with less threads than splits
+            let actual_threads = splits.len() - 1;
+
+            let mut handles = Vec::with_capacity(actual_threads);
+            for i in 0..actual_threads {
                 let mut worker_processor = processor.clone();
                 let start = splits[i];
                 let end = splits[i + 1];
+
                 let handle = scope.spawn(move || {
                     let mut parser = FastxParser::<CONFIG>::from_slice(&data[start..end]);
-                    let mut i = 0;
+                    let mut count = 0;
+
                     while let Some(_event) = parser.next() {
                         worker_processor.process_record(&parser)?;
-                        i += 1;
-                        if i == self.batch_size {
+                        count += 1;
+                        if count == self.batch_size {
                             worker_processor.on_batch_complete()?;
-                            i = 0;
+                            count = 0;
                         }
                     }
-                    if i > 0 {
+                    if count > 0 {
                         worker_processor.on_batch_complete()?;
                     }
                     worker_processor.on_thread_complete()?;
